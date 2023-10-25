c8295046f1a270cd4a4e6cdd4620d305e5b9dee2
AndrejBockaj <bockajandrej@gmail.com>
2023-10-25 11:40:20 +0200

    Opravene posledne chyby a otestovane, pripravene na poslanie do studisu

--- keyfilter.c
+++ keyfilter.c
@@ -8,10 +8,15 @@ char strlwr(char a)
         a += 32;
     return a;
 }
+char strupr(char a){
+    if((a >= 'a') && (a <= 'z'))
+        a -= 32;
+    return a;
+}
 
 void add_char_to_cityname(char cityName[], char input)
 {
-    if (strlen(cityName) < 99)
+    if (strlen(cityName) < 100)
         strncat(cityName, &input, 1);
 }
 
@@ -20,12 +25,13 @@ int count_occur_of_character(char input[], bool printableChar[], char cityName[]
     int c = 0;
     int cntIndex = 0;
     int cntChars = 0;
-    bool enableToFindChar = false;
+    bool enableToFindChar = true;
     bool findChar = false;
     bool ReadFullWord = false;
 
     while ((c = getchar()) != EOF)
     {
+        //printf("Index = %i, Cntchar = %i, getchar = %c, expected = %c\n", cntIndex, cntChars, strupr(c), strupr(input[cntIndex]));
         if (c == 10) // Finding enter character
         {
             if (findChar && (input[cntIndex] == '\0'))
@@ -36,39 +42,39 @@ int count_occur_of_character(char input[], bool printableChar[], char cityName[]
             ReadFullWord = false;
             enableToFindChar = true;
         }
-        else if ((c >= 32) && (c < 127)) // Protect underflow array if c-32 < 0
+        else if ((c >= 32) && (c < 127)) // Protect underflow array if c-32 < 0 on printable char
         {
             if (input == NULL) // Protect for NULL == input
             {
-                if ((cntIndex == 0) && (!(printableChar[c - 32])))
+                if ((cntIndex == 0) && (!(printableChar[strupr(c) - 32])))
                 {
-                    printableChar[c - 32] = true;
+                    printableChar[strupr(c) - 32] = true;
                     cntChars++;
                 }
                 cntIndex++;
             }
 
             else if (ReadFullWord)
-                add_char_to_cityname(cityName, c);
+                add_char_to_cityname(cityName, strupr(c));
             else if ((input[cntIndex] == '\0') && findChar)
             {
-                printableChar[c - 32] = true; // saves char
+                printableChar[strupr(c) - 32] = true; // saves char
                 if (cntChars < 1)
                 {
-                    add_char_to_cityname(cityName, c);
+                    add_char_to_cityname(cityName, strupr(c));
                     ReadFullWord = true;
                 }
                 findChar = false;
                 enableToFindChar = false;
                 cntChars++;
             }
-            else if ((strlwr(c) == strlwr(input[cntIndex])) && enableToFindChar)
+            else if ((strupr(c) == strupr(input[cntIndex])) && enableToFindChar)
             {
                 
                 cntIndex++;
                 findChar = true;
                 if (cntChars < 1)
-                    add_char_to_cityname(cityName, c);
+                    add_char_to_cityname(cityName, strupr(c));
             }
             else
             {
@@ -88,7 +94,7 @@ void print_characters(bool printableChar[])
     for (int index = 32; index < 127; index++)
     {
         if (printableChar[index - 32])
-            printf("%c, ", index);
+            printf("%c", index);
     }
 }
 
@@ -100,8 +106,6 @@ void print_result(int cnt, char cityName[], bool printableChar[])
         printf("Found: %s\n", cityName);
     else
     {
-        if(strlen(cityName) > 1)
-            printf("Found: %s\n", cityName);
         printf("Enable: ");
         print_characters(printableChar);
         printf("\n");
@@ -111,7 +115,7 @@ void print_result(int cnt, char cityName[], bool printableChar[])
 int main(int argc, char **argv)
 {
     bool printableChar[95] = {false}; // (95 = 127chars - 32) chars enable to print
-    char cityName[100];
+    char cityName[100]; 
     int cnt = 0;
 
     (void)argc;
